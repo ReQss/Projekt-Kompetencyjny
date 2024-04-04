a032abc543981ae464a0ab2df9f5eb397eaf2df7
ReQss <ReQsPL@gmail.com>
2024-04-04 18:32:42 +0200

    problem solved

--- rent-backend/src/main/java/com/triptip/triptip/controller/UserController.java
+++ rent-backend/src/main/java/com/triptip/triptip/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+#szysz
 @RestController
 @CrossOrigin ("http://localhost:3000")
 public class UserController {
