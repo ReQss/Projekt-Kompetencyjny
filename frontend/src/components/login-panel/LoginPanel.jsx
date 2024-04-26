44799c0f83f1772fbd24277c29918afe8eb6f97e
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-04-26 10:11:12 +0200

    add login and logout logic

--- frontend/src/components/login-panel/LoginPanel.jsx
+++ frontend/src/components/login-panel/LoginPanel.jsx
@@ -1,21 +1,78 @@
-import React from "react";
-import "./loginPanel.css";
-import logo from "../../assets/logo.png";
-import Button from "../button/Button";
+import { useState } from 'react';
+import './loginPanel.css';
+import logo from '../../assets/logo.png';
+import Button from '../button/Button';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPanel = () => {
+  const [formData, setFormData] = useState({
+    login: '',
+    password: '',
+  });
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    console.log('siema');
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:9192/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      console.log(response);
+      if (response.status === 202) {
+        console.log('Zalogowano pomyślnie!');
+        console.log(response);
+        localStorage.setItem('token', response.data.token);
+        navigate('/');
+        window.location.reload();
+      } else {
+        console.log('Błąd logowania. Spróbuj ponownie.');
+      }
+    } catch (error) {
+      console.error('Błąd podczas wysyłania żądania:', error);
+      console.log('Wystąpił błąd podczas logowania. Spróbuj ponownie później.');
+    }
+  };
+
   return (
     <div className="login_panel">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Logowanie</h1>
-
         <a href="#" className="logo_holder">
           <img src={logo} alt="Logo" />
         </a>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Hasło" />
+        <input
+          type="login"
+          name="login"
+          placeholder="login"
+          value={formData.login}
+          onChange={handleInputChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Hasło"
+          value={formData.password}
+          onChange={handleInputChange}
+          required
+        />
         <a href="#">Zapomniałeś hasła?</a>
-        <Button>Zaloguj się</Button>
+        <Button type="submit">Zaloguj się</Button>
       </form>
     </div>
   );
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,16 +1,33 @@
-import React from "react";
-import "./navbar.css";
-import logo from "../../assets/logo.png";
-import Button from "../button/Button";
+import './navbar.css';
+import logo from '../../assets/logo.png';
+import Button from '../button/Button';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const logoutUser = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('isAdmin');
+    window.location.reload();
+  };
+
   return (
     <div className="navbar">
       <a href="#">
         <img src={logo} alt="Logo" />
       </a>
-      <Link to="/login"><Button> Zaloguj się </Button></Link>
+      <ul>
+        {localStorage.getItem('token') === null ? (
+          <>
+            <Link to="/login">
+              <Button> Zaloguj się </Button>
+            </Link>
+          </>
+        ) : (
+          <li>
+            <button onClick={logoutUser}>Logout</button>
+          </li>
+        )}
+      </ul>
     </div>
   );
 };
