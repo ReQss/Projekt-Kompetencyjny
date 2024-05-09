4c5f42aa9c83211e0604bd1e70925d5826fc472e
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-05-09 14:34:03 +0200

    fetch owner items

--- frontend/src/views/rent/Rent.jsx
+++ frontend/src/views/rent/Rent.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './rent.scss';
 import { Button } from '../../components';
 
@@ -11,6 +11,32 @@ const Rent = () => {
     lastName: '',
     rentDescription: '',
   });
+  const [inventoryList, setInventoryList] = useState([]);
+
+  useEffect(() => {
+    const fetchInventory = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:9192/api/inventoryByOwnerId?ownerId=1'
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          setInventoryList(data);
+        } else {
+          console.error('Failed to fetch inventory');
+        }
+      } catch (error) {
+        console.error('Error fetching inventory:', error);
+      }
+    };
+
+    fetchInventory();
+  }, []);
+
+  useEffect(() => {
+    console.log(inventoryList);
+  }, [inventoryList]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -82,79 +108,88 @@ const Rent = () => {
   };
 
   return (
-    <div className="form-container">
-      <h2>Wypożycz sprzęt</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form">
-          <label htmlFor="inventory">Inventory ID:</label>
-          <input
-            type="number"
-            id="inventory"
-            name="inventory"
-            value={formData.inventory}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="form">
-          <label htmlFor="rentPurpose">Rent Purpose ID:</label>
-          <input
-            type="number"
-            id="rentPurpose"
-            name="rentPurpose"
-            value={formData.rentPurpose}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="form">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="form">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="form">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleInputChange}
-            required
-          />
-        </div>
-        <div className="form">
-          <label htmlFor="rentDescription">Rent Description:</label>
-          <input
-            type="text"
-            id="rentDescription"
-            name="rentDescription"
-            value={formData.rentDescription}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="button">
-          <Button type="submit">Wypożycz</Button>
-        </div>
-      </form>
-    </div>
+    <>
+      <div className="form-container">
+        <h2>Wypożycz sprzęt</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form">
+            <label htmlFor="inventory">Inventory:</label>
+            <select
+              id="inventory"
+              name="inventory"
+              value={formData.inventory}
+              onChange={handleInputChange}
+              required
+            >
+              <option value="">Wybierz produkt</option>
+              {inventoryList.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.itemName}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="form">
+            <label htmlFor="rentPurpose">Rent Purpose ID:</label>
+            <input
+              type="number"
+              id="rentPurpose"
+              name="rentPurpose"
+              value={formData.rentPurpose}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form">
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form">
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div className="form">
+            <label htmlFor="rentDescription">Rent Description:</label>
+            <input
+              type="text"
+              id="rentDescription"
+              name="rentDescription"
+              value={formData.rentDescription}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="button">
+            <Button type="submit">Wypożycz</Button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
