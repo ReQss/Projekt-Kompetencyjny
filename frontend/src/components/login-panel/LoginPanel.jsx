9a2759ece6e4a27f8b62da28064d34947e9141b5
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-05-22 20:45:40 +0200

    cleaning

--- frontend/src/components/login-panel/LoginPanel.jsx
+++ frontend/src/components/login-panel/LoginPanel.jsx
@@ -10,7 +10,6 @@ const LoginPanel = () => {
     password: '',
   });
   const [error, setError] = useState('');
-  const [userId, setUserId] = useState(null);
   const navigate = useNavigate();
 
   const handleInputChange = (e) => {
@@ -44,7 +43,6 @@ const LoginPanel = () => {
         localStorage.setItem('userId', data.user.id);
         localStorage.setItem('email', data.user.email);
         localStorage.setItem('role', data.user.role);
-        setUserId(data.userId);
         navigate('/');
       } else if (response.status === 401) {
         console.log('Login error:', data.error);
