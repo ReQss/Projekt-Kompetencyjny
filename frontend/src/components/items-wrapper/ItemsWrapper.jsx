cc0d0179e721604bdeb364ed43d2a4ef59e83cf6
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-05-23 10:05:57 +0200

    check is user admin and delete for whole invetory

--- frontend/src/components/items-wrapper/ItemsWrapper.jsx
+++ frontend/src/components/items-wrapper/ItemsWrapper.jsx
@@ -8,48 +8,51 @@ const ItemsWrapper = () => {
   const [items, setItems] = useState([]);
 
   const location = useLocation();
-  
+
   const loggedInUserId = localStorage.getItem('userId');
+  const userRole = localStorage.getItem('role');
+  console.log(userRole);
 
-  const filteredItems = items.filter(item => {
-    if (location.pathname === '/modify') {
+  const filteredItems = items.filter((item) => {
+    if (userRole === 'ADMIN') {
+      console.log('full access');
+    } else if (location.pathname === '/modify') {
       return item.ownerId === parseInt(loggedInUserId);
-
     } else if (location.pathname === '/delete') {
       return item.ownerId === parseInt(loggedInUserId);
-    } 
+    }
     // W innych przypadkach zwróć wszystkie przedmioty
-
-    
-
     return true;
   });
 
   useEffect(() => {
     fetch('http://localhost:9192/api/inventory')
-     .then(response => response.json())
-     .then(data => {
-        const processedData = data.map(item => {
+      .then((response) => response.json())
+      .then((data) => {
+        const processedData = data.map((item) => {
           if (typeof item.category === 'object' && item.category !== null) {
             return {
-             ...item,
-             category: item.category.name
+              ...item,
+              category: item.category.name,
             };
           }
           return item;
         });
         setItems(processedData);
       })
-     .catch(error => console.error('Error fetching items:', error));
+      .catch((error) => console.error('Error fetching items:', error));
   }, []);
 
   return (
-    <div className='items-wrapper'>
-      {filteredItems.map(item => (
-        <Item 
+    <div className="items-wrapper">
+      {console.log(filteredItems)}
+      {filteredItems.map((item) => (
+        <Item
           item={item}
           key={item.id}
-          src={item.photo ? `data:image/jpeg;base64,${item.photo}` : komputerImage} // Zakładając, że klucz to 'photo' i wartość to base64
+          src={
+            item.photo ? `data:image/jpeg;base64,${item.photo}` : komputerImage
+          } // Zakładając, że klucz to 'photo' i wartość to base64
           id={item.id}
           name={item.itemName}
           description={item.description}
