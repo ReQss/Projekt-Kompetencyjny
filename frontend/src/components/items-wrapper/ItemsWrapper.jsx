e438763e21980fccc0ed3002dca6d735af2cfb25
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2024-05-23 10:06:09 +0200

    cleaning

--- frontend/src/components/items-wrapper/ItemsWrapper.jsx
+++ frontend/src/components/items-wrapper/ItemsWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './itemsWrapper.css';
 import Item from '../item/Item';
 import komputerImage from '../../assets/komputer.jpg';
